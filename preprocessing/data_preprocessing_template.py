c6abc9e0e3f4e651b798212bf8a8963b55fe94b9
curtisnoel <curtis.noel@gmail.com>
2016-11-18 00:23:58 -0500

    added categorization

--- preprocessing/data_preprocessing_template.py
+++ preprocessing/data_preprocessing_template.py
@@ -20,3 +20,7 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
