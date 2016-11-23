fc00e87ed29300e24c273dd5d2abf719d5673a5b
curtisnoel <curtis.noel@gmail.com>
2016-11-22 20:18:58 -0500

    added scaling, reformatted to a template

--- preprocessing/data_preprocessing_template.py
+++ preprocessing/data_preprocessing_template.py
@@ -11,20 +11,26 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 #take care of missing data
-from sklearn.preprocessing import Imputer
+"""from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy='mean', axis = 0)
 imputer = imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])"""
 
 #encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
 x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 x = onehotencoder.fit_transform(x).toarray()
 labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+y = labelencoder_y.fit_transform(y)"""
 
 #split test and train datasets
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+#scale
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.fit_transform(x_test)"""
